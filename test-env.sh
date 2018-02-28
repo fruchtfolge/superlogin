2f22e46ab49d4ee1c2983c5505d4d9fa2976679e
PhilLaFayette <PJL@MHElectricMotors.com>
2018-02-28 12:15:16 -0600

    Added test-env.sh

---
+++ test-env.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Local Environment Variables for running package tests
+
+export COUCH_PROTOCOL='http://'
+export COUCH_HOST='localhost:5984'
+export COUCH_USER='sladmin'
+export COUCH_PASS='sladminpass'
+export REDIS_HOST='localhost'
+export REDIS_PASSWORD='redispassword'
